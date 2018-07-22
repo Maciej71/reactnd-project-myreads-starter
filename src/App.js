a85a390667cb38384ab88ca0e56a959e60b45c72
Maciej Giza <maciejgiza@gmail.com>
2018-07-22 17:23:34 +0200

    Change content fetching from static to API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Shelves from './Shelves'
 import Search from './Search'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <Shelves/>
+          <Shelves
+            books={this.state.books}
+          />
         )}/>
 
         <Route path="/search" render={() => (
---
+++ src/Book.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Book extends Component {
+    static propTypes = {
+        book: PropTypes.object.isRequired
+    }
+
+    render() {
+        const {book} = this.props
+        let background = book.imageLinks ? `url(${book.imageLinks.thumbnail})` : '#fff'
+
+        return(
+            //display a book
+            <div className="book">
+                <div className="book-top">
+                <div className="book-cover" style={{ backgroundImage: `${background}` }}></div>
+                <div className="book-shelf-changer">
+                    <select>
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                    </select>
+                </div>
+                </div>
+                <div className="book-title">{book.title ? book.title : 'Title not Available'}</div>
+                <div className="book-authors">{book.authors ? book.authors.join(', ') : 'Author not available'}</div>
+            </div>
+        )
+    }
+}
+
+export default Book
